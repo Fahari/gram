876ed81d315abbff0f9ff000c4c628626f9fba1c
Fahari <karonjekevin67@gmail.com>
2019-03-11 10:01:26 +0300

    create an Image model and test its instance

--- gram/models.py
+++ gram/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+    image = models.ImageField(upload_to = 'images/')
+    name = models.CharField(max_length =30)
+    caption = models.TextField(max_length =120)
--- gram/tests.py
+++ gram/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from .models import Image
 
 # Create your tests here.
+class ImageTestClass(TestCase):
+    def setUp(self):
+        self.photo= Image(name = 'black',caption ='black man')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.photo,Image))
