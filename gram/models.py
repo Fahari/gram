6cc27f9b35a21e6b7033c7ad4bcab16687957cdc
Fahari <karonjekevin67@gmail.com>
2019-03-11 10:06:21 +0300

    ran and passed the save image test

--- gram/models.py
+++ gram/models.py
@@ -5,3 +5,9 @@ class Image(models.Model):
     image = models.ImageField(upload_to = 'images/')
     name = models.CharField(max_length =30)
     caption = models.TextField(max_length =120)
+
+    def __str__(self):
+        return self.name
+
+    def save_image(self):
+        self.save()
