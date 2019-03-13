3ffa26020764995363b8c9ca8393101a1a563641
Fahari <karonjekevin67@gmail.com>
2019-03-13 03:49:43 +0300

    create Profile models

--- gram/models.py
+++ gram/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Image(models.Model):
@@ -15,3 +16,10 @@ class Image(models.Model):
     def search_by_user(cls,search_term):
         users = cls.objects.filter(title__icontains=search_term)
         return users
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image=models.ImageField(default='default.jpg', upload_to='profile_pics')
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
